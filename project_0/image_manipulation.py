57c68aa28e5058019260d99eef3b7afeacdd0973
Max Timkovich <mtimkovich@gmail.com>
2014-08-29 15:14:05 -0700

    Stuff is working

--- project_0/image_manipulation.py
+++ project_0/image_manipulation.py
@@ -7,5 +7,32 @@ Use the unit tests is image_manipulation_test.py to guide
 your implementation, adding functions as needed until all
 unit tests pass.
 """
+import cv2
+import numpy as np
 
-# TODO: Implement!
+
+def flip_image(img, horizontal, vertical):
+    flip_code = None
+
+    if horizontal and vertical:
+        flip_code = -1
+    elif horizontal:
+        flip_code = 1
+    elif vertical:
+        flip_code = 0
+
+    if flip_code is not None:
+        return cv2.flip(img, flip_code)
+    else:
+        return img
+
+
+def negate_image(img):
+    return cv2.bitwise_not(img)
+
+
+def swap_blue_and_green(img):
+    ch = cv2.split(img)
+    swapped = cv2.merge([ch[1], ch[0], ch[2]])
+
+    return swapped
