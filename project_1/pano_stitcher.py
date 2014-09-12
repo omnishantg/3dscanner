f18576cc4c92afdb13be251a4ba6161395469019
huntinglauren <hunter7@cs.utexas.edu>
2014-09-11 19:36:06 -0500

    homography

--- project_1/pano_stitcher.py
+++ project_1/pano_stitcher.py
@@ -21,6 +21,34 @@ def homography(image_a, image_b, bff_match=False):
     Returns: the 3x3 perspective transformation matrix (aka homography)
              mapping points in image_b to corresponding points in image_a.
     """
+    #initiate SIFT detector
+    orb = cv2.ORB()
+
+    #find the keypoints and descriptors with SIFT
+    kp1, des1 = orb.detectAndCompute(image_a, None)
+    kp2, des2 = orb.detectAndCompute(image_b, None)
+
+    #create BFMatcher object and find matches
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
+    matches = bf.match(des1,des2)
+
+    #sort through matches to find good matches based on Lowe's ration test
+    good = []
+    for m,n in matches:
+      if m.distance < 0.7 * n.distance:
+        good.append(m)
+    
+    if len(good) > 10:
+      src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+      dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+
+    else: 
+        matchesMask = None
+
+    #find homography matrix
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    return M
+
     pass
 
 
