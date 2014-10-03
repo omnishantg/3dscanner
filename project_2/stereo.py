c6dae2eadb596f1df374359507d358117f2027ab
Max Timkovich <mtimkvch@cs.utexas.edu>
2014-10-03 15:52:17 -0500

    Returning something for retify images

--- project_2/stereo.py
+++ project_2/stereo.py
@@ -5,7 +5,7 @@ In this project, you'll extract dense 3D information from stereo image pairs.
 
 import cv2
 import math
-import numpy
+import numpy as np
 
 
 def rectify_pair(image_left, image_right, viz=False):
@@ -19,7 +19,45 @@ def rectify_pair(image_left, image_right, viz=False):
       H_left, H_right: homographies that warp the left and right image so
         their epipolar lines are corresponding rows.
     """
-    pass
+
+    # Detect keypoints in both images using SIFT
+    sift = cv2.SIFT()
+    kp_left, des_left = sift.detectAndCompute(image_left, None)
+    kp_right, des_right = sift.detectAndCompute(image_right, None)
+
+    # FLANN parameters
+    FLANN_INDEX_KDTREE = 0
+    NUM_TREES = 5
+    NUM_CHECKS = 100
+
+    # Find matches between keypoints using FLANN
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=NUM_TREES)
+    search_params = dict(checks=NUM_CHECKS)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des_left, des_right, k=2)
+
+    THRESHOLD = 0.75
+    good = [m for m, n in matches if m.distance < THRESHOLD * n.distance]
+
+    # Return None if there are not enough matches between the two images
+    if len(good) < 3:
+        return None
+
+    left_pts = np.float32([kp_left[m.queryIdx].pt for m in good])
+    right_pts = np.float32([kp_right[m.trainIdx].pt for m in good])
+
+    F, status = cv2.findFundamentalMat(left_pts, right_pts)
+
+    status = status.ravel()
+    mp1 = left_pts[status].reshape(1, -1, 2)
+    mp2 = right_pts[status].reshape(1, -1, 2)
+
+    _, H_left, H_right = cv2.stereoRectifyUncalibrated(mp1,
+                                                       mp2,
+                                                       F,
+                                                       image_left.shape[:2])
+
+    return (F, H_left, H_right)
 
 
 def disparity_map(image_left, image_right):
